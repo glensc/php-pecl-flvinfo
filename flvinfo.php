44adbeabc148e1a87fbb2b2135ed7d7689f7a38c
Elan Ruusamäe <glen@delfi.ee>
2006-01-03 18:32:43 +0000

    - allow cli specify different file to check
    Changed files:
        flvinfo.php -> 1.5

--- flvinfo.php
+++ flvinfo.php
@@ -1,22 +1,14 @@
+#!/usr/bin/php
 <?php
-$br = (php_sapi_name() == 'cli') ? '' : '<br>';
+$file = ($argc > 1 ? $argv[1] : "fall2005.flv");
 
-if(!extension_loaded('flvinfo')) {
+if (!extension_loaded('flvinfo')) {
 	dl('flvinfo.' . PHP_SHLIB_SUFFIX);
 }
-$module = 'flvinfo';
-$functions = get_extension_funcs($module);
-echo "Functions available in the test extension:$br\n";
-foreach($functions as $func) {
-    echo $func."$br\n";
-}
-echo "$br\n";
 
-$res = get_flv_dimensions("fall2005.flv");
+$res = get_flv_dimensions($file);
 if (!isset($res)) {
 	echo "res is not set\n";
 }
 echo "res=[$res]\n";
 print_r($res);
-
-?>
