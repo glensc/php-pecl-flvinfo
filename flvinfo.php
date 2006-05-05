5ed21443930c0aa3a9b05110a73e06abd2624043
Elan Ruusamäe <glen@delfi.ee>
2006-05-05 12:56:26 +0000

    - force php4 test
    Changed files:
        flvinfo.php -> 1.6

--- flvinfo.php
+++ flvinfo.php
@@ -1,8 +1,9 @@
-#!/usr/bin/php
+#!/usr/bin/php4.cli
 <?php
 $file = ($argc > 1 ? $argv[1] : "fall2005.flv");
 
 if (!extension_loaded('flvinfo')) {
+	echo "loading extension\n";
 	dl('flvinfo.' . PHP_SHLIB_SUFFIX);
 }
 
