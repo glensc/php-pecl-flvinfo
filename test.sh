327d11728effd4609ce03d08285a41571c943bf9
Elan Ruusamäe <glen@delfi.ee>
2006-12-08 16:55:29 +0000

    - php5 test
    Changed files:
        test.sh -> 1.2

--- test.sh
+++ test.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
-php4.cli -dextension_dir=modules flvinfo.php "$@"
+PHP=php.cli
+#PHP=php4.cli
+FILE=/tmp/Krimiuudised_20061204_jegorovi_mure.flv
+$PHP -dextension_dir=modules flvinfo.php $FILE
